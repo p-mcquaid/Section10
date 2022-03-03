55c292fd7d77bf6e3129d54438196f86680bf81e
p-mcquaid <49034399+p-mcquaid@users.noreply.github.com>
2022-03-03 19:37:46 +0000

    Solution to SectionChallenge

--- SectionChallenge/main.cpp
+++ SectionChallenge/main.cpp
@@ -12,25 +12,48 @@ display new encrypted word
 reverse process to un-encrypt word
 
 */
-int main(){
+int main()
+{
 
     string word{"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
     string key{"qazwsxedcrfvtgbyhnujmikolpQAZWSXEDCRFVTGBYHNUJMIKOLP"};
     string phrase{};
 
     string encodedWord{};
-
+    string decryptWord{};
     cout << "Please enter your secret phrase: ";
-    getline(cin,phrase);
-
-    for (auto c : phrase)
+    getline(cin, phrase);
+    // Encryption
+    for (char c : phrase)
     {
+
         size_t pos = word.find(c);
-        cout << pos << endl;
-        encodedWord = key.at(pos);
+        if (pos != string::npos)
+        {
+            encodedWord += key.at(pos);
+        }
+        else
+        {
+            encodedWord += c;
+        }
     }
     cout << encodedWord << endl;
-    
-    
+
+    // Decryption
+    cout << "Decrypting message..." << endl;
+    for (auto c : encodedWord)
+    {
+        size_t pos = key.find(c);
+        if (pos != string::npos)
+        {
+            decryptWord += word.at(pos);
+        }
+        else
+        {
+            decryptWord += c;
+        }
+    }
+    cout << decryptWord << endl;
+
     return 0;
-} 
+}
